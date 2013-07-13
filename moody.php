e2716bd638f0e57d685c073a87367b6db6ef9f21
spandana <spandana@spandana-Dell-System-XPS-L502X.(none)>
2013-07-13 20:11:46 +0530

    basic moody and unbasic tag

--- moody.php
+++ moody.php
@@ -1,25 +1,22 @@
 <?php
-
-//$query = $_POST["query"];
-$query="awesome";
-$con = mysql_connect("localhost","root","mysql123","moody");
-$page = "<html>
-<head>
-<title>Moody</title>
-</head>
-<body>
-<form method=\"post\" align=\"center\" action=\"moody.php\">
-<input type=\"text\" id=\"query\">
-</form>";
-$sql_query = 'SELECT * FROM main WHERE tag="'.$query.'" ORDER BY count DESC';
-$result = mysql_query(string($con), $sql_query);
-while($row = mysql_fetch_array($result))
+$query = $_POST["query"];
+//echo $query;
+$con = mysqli_connect("localhost","root","spaspa","moody");
+if (mysqli_connect_errno($con))
 {
-	$video = '<iframe width="420" height="345"
-		src="'.$row['url'].'"></iframe>';
-	$page = $page.$video;
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-$page = $page."</body></html>";
-mysql_close($con);
-Echo $page;
+$sqlquery = "SELECT * FROM main WHERE tag= \"".$query."\" ORDER BY count DESC";
+//echo $sqlquery;
+$result = mysqli_query($con,$sqlquery);
+Echo "<html>";
+Echo "<body>";
+while($row = mysqli_fetch_array($result))
+{
+$row['url'] = str_replace("watch?v=","embed/",$row['url']);
+Echo "<iframe width='420' height='345' src=\"".$row['url']."\">
+</iframe>";
+Echo "</br>";
+}
+mysqli_close($con);
 ?>
